cb9396a28d55c9ab25a3069161123791ffba86d4
rafael <rafael.barbosa@unimedrecife.com.br>
2024-04-08 14:21:30 -0300

    adicionando efeitor

--- introduction.py
+++ introduction.py
@@ -1,2 +1,11 @@
+from streamlit_extras.let_it_rain import rain 
 import streamlit as st
 st.title("Introduction")
+
+def example():
+    rain(
+        emoji="📔,📕,📗,📘,📙",
+        font_size=54,
+        falling_speed=5,
+        animation_length="infinite",
+    )
